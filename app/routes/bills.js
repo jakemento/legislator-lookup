68553bad730de31936afcc378ee3debd6e73085c
jake steinberg <jakesteinberg@gmail.com>
2016-04-05 15:54:54 -0700

    get rid of max = 20 for listing bills and committees

--- app/routes/bills.js
+++ app/routes/bills.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    var url= 'http://congress.api.sunlightfoundation.com/bills?history.active=true&order=last_action_at&apikey=cb6243441c5544de8ee26c2779e02061';
+    var url= 'http://congress.api.sunlightfoundation.com/bills?per_page=50&history.active=true&order=last_action_at&apikey=cb6243441c5544de8ee26c2779e02061';
       return Ember.$.getJSON(url).then(function(responseJSON) {
         return responseJSON.results;
       });
--- app/routes/committees.js
+++ app/routes/committees.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    var url= 'http://congress.api.sunlightfoundation.com/committees?subcommittee=true&apikey=cb6243441c5544de8ee26c2779e02061';
+    var url= 'http://congress.api.sunlightfoundation.com/committees?per_page=all&subcommittee=true&apikey=cb6243441c5544de8ee26c2779e02061';
       return Ember.$.getJSON(url).then(function(responseJSON) {
         return responseJSON.results;
       });
