220bc49aa443701db387832424ecfcbbceb638b3
Andrew Telleria and jake steinberg <jake@>
2016-04-05 10:50:20 -0700

    add button to show recent bills, and show committees

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     zipLookup(params) {
-      this.transitionTo('results', params.zip)
+      this.transitionTo('results', params.zip);
+    },
+    listCommittees() {
+      this.transitionTo('committees');
+    },
+    listBills() {
+      this.transitionTo('bills');
     }
   }
 });
