291d8b05cb1eb0294cd510d15c199b34f9531cd4
Andrew Telleria and jake steinberg <jake@>
2016-04-05 14:03:14 -0700

    add viewable pages for each specfic bill

--- app/routes/bill.js
+++ app/routes/bill.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    var url= 'http://congress.api.sunlightfoundation.com/bills?history.active=true&order=last_action_at&apikey=cb6243441c5544de8ee26c2779e02061';
+  model: function(params){
+    var id = params.bill_id;
+    var url= 'http://congress.api.sunlightfoundation.com/bills?bill_id=' + id + '&apikey=cb6243441c5544de8ee26c2779e02061';
       return Ember.$.getJSON(url).then(function(responseJSON) {
         return responseJSON.results;
       });
